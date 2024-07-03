4035a2fafe1da02068cfa8366294da9d44a0aa8d
joanmera <jamerar@puce.edu.ec>
2024-07-02 21:34:19 -0500

    corrected

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,74 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
 import './style.css';
 
 function Home() {
   const [data, setData] = useState({
-    celcius: 10,
-    name: 'London',
-    humidity: 10,
-    speed: 2
+    celcius: null,
+    name: '',
+    humidity: null,
+    speed: null,
+    image: ''
   });
-  
+
   const [name, setName] = useState('');
+  const [error, setError] = useState('');
+
+  const fetchWeather = (city) => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1240f8bbd692f924a6f2aba0539841f1&units=metric`;
+
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Ciudad invalida');
+        }
+        return response.json();
+      })
+      .then(res => {
+        console.log(res); // Verificar que res contiene los datos esperados
+        let imagePath = '';
+        switch (res.weather[0].main) {
+          case "Clouds":
+            imagePath = '/images/clouds.png';
+            break;
+          case "Clear":
+            imagePath = '/images/clear.png';
+            break;
+          case "Rain":
+            imagePath = '/images/rain.png';
+            break;
+          case "Drizzle":
+            imagePath = '/images/drizzle.png';
+            break;
+          case "Mist":
+            imagePath = '/images/mist.png';
+            break;
+          default:
+            imagePath = '/images/clouds.png';
+            break;
+        }
+
+        setData({
+          celcius: res.main.temp,
+          name: res.name,
+          humidity: res.main.humidity,
+          speed: res.wind.speed,
+          image: imagePath
+        });
+        setError('');
+      })
+      .catch(err => {
+        setError(err.message);
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchWeather('London');
+  }, []);
 
   const handleClick = () => {
     if (name !== "") {
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=1240f8bbd692f924a6f2aba0539841f1&units=metric`;
-      axios.get(apiUrl)
-        .then(res => {
-          console.log(res.data);
-          setData({
-            ...data,
-            celcius: res.data.main.temp,
-            name: res.data.name,
-            humidity: res.data.main.humidity,
-            speed: res.data.wind.speed
-          });
-        })
-        .catch(error => console.log(error));
+      fetchWeather(name);
     }
   };
 
@@ -43,15 +85,20 @@ function Home() {
             <img src="/images/search.png" alt="Buscar" />
           </button>
         </div>
+        {error && (
+          <div className="error">
+            <p>{error}</p>
+          </div>
+        )}
         <div className="winfo">
-          <img className="clouds" src="/images/clouds.png" alt="Clouds" />
+          <img className="clouds" src={data.image} alt="Weather" />
           <h1>{Math.round(data.celcius)} °C</h1>
           <h2>{data.name}</h2>
           <div className="details">
             <div className="col">
               <img src="/images/humidity.png" alt="Humidity" />
               <div className="humidity">
-                <p>{Math.round(data.humidity)}</p>
+                <p>{Math.round(data.humidity)}%</p>
                 <p>Humedad</p>
               </div>
             </div>
